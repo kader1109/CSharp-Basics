2e1e8ab6c7bba7b3a9fbceacf9eb4bf53f9026bd
kader1109 <51703456+kader1109@users.noreply.github.com>
2025-10-14 10:36:46 +0300

    Unit 01. First Steps- Lab

---
+++ 01.FirstStep-Lab/05.GreetingByName/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _05.GreetingByName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
---
+++ 01.FirstStep-Lab/08.PetShop/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _08.PetShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double dogFood = 2.5;
+            double catFood = 4;
+
+            int pacetDogFood = int.Parse(Console.ReadLine());
+            int pacetCatFood = int.Parse(Console.ReadLine());
+
+            double sum = (dogFood * pacetDogFood) + (catFood * pacetCatFood);
+            Console.WriteLine($"{sum} lv.");
+        }
+    }
+}
