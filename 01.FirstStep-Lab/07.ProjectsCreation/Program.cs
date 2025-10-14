e2dc18eaacdba784589e7b9d9aa33031a8cc3d48
kader1109 <51703456+kader1109@users.noreply.github.com>
2025-10-14 10:01:33 +0300

    Unit 01. First steps

---
+++ 01.FirstStep-Lab/07.ProjectsCreation/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _07.ProjectsCreation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string architectName = Console.ReadLine();
+            int projectsCount = int.Parse(Console.ReadLine());
+
+            int hourForProjects = projectsCount * 3;
+
+            Console.WriteLine($"The architect {architectName} will need {hourForProjects} hours to complete {projectsCount} project/s.");
+
+        }
+    }
+}
