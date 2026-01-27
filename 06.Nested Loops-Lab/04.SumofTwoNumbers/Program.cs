cb21fe44172b8580fc8d088044400533a32ac998
kader1109 <51703456+kader1109@users.noreply.github.com>
2026-01-27 10:43:16 +0200

    06.Nested Loops - Lab

---
+++ 06.Nested Loops-Lab/04.SumofTwoNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _04.SumofTwoNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startOfTheInterval = int.Parse(Console.ReadLine());
+            int endOfTheInterval = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+
+            int combinationCount = 0;
+
+            for (int i = startOfTheInterval; i <= endOfTheInterval; i++)
+            {
+                for (int j = startOfTheInterval; j <= endOfTheInterval; j++)
+                {
+                    combinationCount++;
+
+                    if (i + j == magicNumber)
+                    {
+                        Console.WriteLine($"Combination N:{combinationCount} ({i}" +
+                            $" + {j} = {magicNumber})");
+                        return;
+
+                    }
+                }
+            }
+            Console.WriteLine($"{combinationCount} combinations - neither equals {magicNumber}");
+        }
+
+    }
+}
+
+
---
+++ 06.Nested Loops-Lab/06.Building/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _06.Building
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfFloor = int.Parse(Console.ReadLine());
+            int numberOfRooms = int.Parse(Console.ReadLine());
+
+            for (int floor = numberOfFloor; floor >= 1; floor--)
+            {
+                for (int room = 0 ; room < numberOfRooms; room++)
+                {
+                    if (floor == numberOfFloor)
+                    {
+                        Console.Write($"L{floor}{room} ");
+
+                    }
+
+                    else if (floor % 2 == 0)
+                    {
+
+                        Console.Write($"O{floor}{room} ");
+
+                    }
+                    else
+                    {
+
+                        Console.Write($"A{floor}{room} ");
+                    }
+                }
+                Console.WriteLine();
+                
+            }
+        }
+    }
+}
